d494b89617a2c2353648b38c7ed502de34069b24
Tatiana Yarchenko <ktanya61@gmail.com>
2022-04-25 17:43:23 +0300

    HW-7 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+// console.log(createGalleryItemsMarkup(galleryItems));
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>;
+  `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(e) {
+  const isGalleryImageEl = e.target.classList.contains("gallery__image");
+
+  if (!isGalleryImageEl) {
+    return;
+  }
+  e.preventDefault();
+
+  const largeImage = e.target.dataset.source;
+  console.log(largeImage);
+
+  //   const instance = basicLightbox.create(`
+  //     <img src="${largeImage}">
+  // `);
+
+  //   instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>;
+  `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(e) {
+  const isGalleryImageEl = e.target.classList.contains("gallery__image");
+
+  if (!isGalleryImageEl) {
+    return;
+  }
+
+  console.log(isGalleryImageEl);
+  e.preventDefault();
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionPosition: "bottom",
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  console.log(lightbox);
+}
